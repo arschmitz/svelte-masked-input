3f0d09ac9c3ef26b20b18f1b0f509c968c3d7462
Alexander Schmitz <arschmitz@gmail.com>
2023-03-15 01:24:49 -0400

    fixup

--- src/helpers.ts
+++ src/helpers.ts
@@ -23,6 +23,7 @@ export type BaseFormatObject = Record<Format, FormatFunction>;
 export interface GetFormatPartsOptions {
     currency: string;
     locale: string;
+    type: DecimalFormat;
 }
 
 export interface FormatInput {
